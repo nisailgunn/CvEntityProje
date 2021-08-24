9e35a87518a5b9abf16c2db3e83613dd8b41cce7
nisailgunn <nisailgyn@gmail.com>
2021-08-24 10:10:11 +0300

    CvEntityProje

---
+++ Default.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvEntityProje
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        DBCVENTITYEntities db = new DBCVENTITYEntities();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Repeater1.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater1.DataBind();
+            Repeater2.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater2.DataBind();
+            Repeater3.DataSource = db.TBLHAKKIMDA.ToList();
+            Repeater3.DataBind();
+            Repeater4.DataSource = db.TBLYETENEKLER.ToList();
+            Repeater4.DataBind();
+
+
+
+
+        }
+    }
+}
